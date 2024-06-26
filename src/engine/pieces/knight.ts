025b39b0179dddce2d0e503cb58240f323d2724c
David McIntosh <David.McIntosh@Softwire.com>
2024-06-26 17:06:15 +0100

    Knights move

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,13 +1,42 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import gameSettings from "../gameSettings";
+import Square from "../square";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
+    public getDiagonalMoves(board: Board) {
+        const currentSquare = board.findPiece(this);
+        let moves = [];
+        for (let i = -gameSettings.BOARD_SIZE; i < gameSettings.BOARD_SIZE; i++) {
+            moves.push(new Square(currentSquare.row + i, currentSquare.col + i));
+            moves.push(new Square(currentSquare.row + i, currentSquare.col - i));
+        }
+
+        moves = moves.filter(this.inBounds);
+        moves = moves.filter((x) => currentSquare.notEquals(x));
+        return moves;
+    }
+
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        let moves = [];
+
+        moves.push(new Square(currentSquare.row + 2, currentSquare.col + 1));
+        moves.push(new Square(currentSquare.row + 2, currentSquare.col - 1));
+        moves.push(new Square(currentSquare.row - 2, currentSquare.col + 1));
+        moves.push(new Square(currentSquare.row - 2, currentSquare.col - 1));
+        moves.push(new Square(currentSquare.row + 1, currentSquare.col + 2));
+        moves.push(new Square(currentSquare.row + 1, currentSquare.col - 2));
+        moves.push(new Square(currentSquare.row - 1, currentSquare.col + 2));
+        moves.push(new Square(currentSquare.row - 1, currentSquare.col - 2));
+
+        moves = moves.filter(this.inBounds);
+
+        return moves;
     }
 }
