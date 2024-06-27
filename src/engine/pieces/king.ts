1d0875ff1fcffffbfe1304c0792ee2893ae06050
David McIntosh <David.McIntosh@Softwire.com>
2024-06-27 11:17:52 +0100

    Added moves for king

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Offset from "../offset";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,23 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        let moves = [];
+        let directions = [
+            Offset.north(),
+            Offset.south(),
+            Offset.east(),
+            Offset.west(),
+            Offset.northeast(),
+            Offset.northwest(),
+            Offset.southeast(),
+            Offset.southwest()
+
+        ]
+        for(const direction of directions){
+            moves.push(currentSquare.squareAtOffset(direction));
+        }
+
+        return moves;
     }
 }
